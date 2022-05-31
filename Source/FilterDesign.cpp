05ef8d43a2bbdd39784872b7b4e68684236d80f3
Phil C <philcsound@gmail.com>
2022-05-31 15:25:22 -0500

    Changed filterdesign to take &params
    Slight optimization

--- Source/FilterDesign.cpp
+++ Source/FilterDesign.cpp
@@ -1,6 +1,6 @@
 #include "FilterDesign.h"
 
-FilterCoef FilterDesign::GetFirstOrderHighpass(FilterParameters params)
+FilterCoef FilterDesign::GetFirstOrderHighpass(FilterParameters& params)
 {
 	double w0 = M_PI * params.getCutoff() / params.getSampleRate();
 	double a1, b0;
@@ -10,7 +10,7 @@ FilterCoef FilterDesign::GetFirstOrderHighpass(FilterParameters params)
 	return FilterCoef(1, a1, 0.0f, b0, -b0, 0.0f);
 }
 
-FilterCoef FilterDesign::GetFirstOrderLowpass(FilterParameters params)
+FilterCoef FilterDesign::GetFirstOrderLowpass(FilterParameters& params)
 {
 	double w0 = M_PI * params.getCutoff() / params.getSampleRate();
 	double a1, b0;
@@ -20,7 +20,7 @@ FilterCoef FilterDesign::GetFirstOrderLowpass(FilterParameters params)
 	return FilterCoef(1, a1, 0.0f, b0, b0, 0.0f);
 }
 
-FilterCoef FilterDesign::GetSecondOrderHighpass(FilterParameters params)
+FilterCoef FilterDesign::GetSecondOrderHighpass(FilterParameters& params)
 {
 	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
 	double sinW0 = std::sin(w0);
@@ -35,7 +35,7 @@ FilterCoef FilterDesign::GetSecondOrderHighpass(FilterParameters params)
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
-FilterCoef FilterDesign::GetSecondOrderLowpass(FilterParameters params)
+FilterCoef FilterDesign::GetSecondOrderLowpass(FilterParameters& params)
 {
 	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
 	double sinW0 = std::sin(w0);
@@ -52,7 +52,7 @@ FilterCoef FilterDesign::GetSecondOrderLowpass(FilterParameters params)
 
 //Returns coefs for 12/24/36/48db flat filters
 //Cascaded num = index of filter, num2ndOrder is max num biquads cascading ex: 48db is 4 biquads
-FilterCoef FilterDesign::CascadeSecondOrderHighpass(FilterParameters params, int cascadedNum, unsigned int num2ndOrdFilter)
+FilterCoef FilterDesign::CascadeSecondOrderHighpass(FilterParameters& params, int cascadedNum, unsigned int num2ndOrdFilter)
 {
 	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
 	double sinW0 = std::sin(w0);
@@ -67,12 +67,13 @@ FilterCoef FilterDesign::CascadeSecondOrderHighpass(FilterParameters params, int
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
-FilterCoef FilterDesign::CascadeSecondOrderLowpass(FilterParameters params, int cascadedNum, unsigned int num2ndOrdFilter)
+FilterCoef FilterDesign::CascadeSecondOrderLowpass(FilterParameters& params, int cascadedNum, unsigned int num2ndOrdFilter)
 {
 	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
 	double sinW0 = std::sin(w0);
 	double cosW0 = std::cos(w0);
-	double alpha = sinW0 / (2 * std::cos((1 + (cascadedNum * 2)) *  M_PI / (4 * num2ndOrdFilter)));
+	double alphaTheta = (1 + (cascadedNum * 2)) *  M_PI / (4 * num2ndOrdFilter);
+	double alpha = sinW0 / (2 * std::cos(alphaTheta));
 	double a0, a1, a2, b0, b1;	//B2 = b0, so
 	a0 = 1 / (1 + alpha);		//Did reciprical so I can multiply instead of divide
 	a1 = (-2 * cosW0) * a0;
@@ -82,7 +83,7 @@ FilterCoef FilterDesign::CascadeSecondOrderLowpass(FilterParameters params, int 
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
-FilterCoef FilterDesign::Cascade1stAnd2ndOrderHighpass(FilterParameters params, int cascadedNum, unsigned int numBiquads)
+FilterCoef FilterDesign::Cascade1stAnd2ndOrderHighpass(FilterParameters& params, int cascadedNum, unsigned int numBiquads)
 {
 	if (!cascadedNum)	//The first filter in a 18/30/42 filter is a first order filter
 		return GetFirstOrderHighpass(params);
@@ -90,7 +91,8 @@ FilterCoef FilterDesign::Cascade1stAnd2ndOrderHighpass(FilterParameters params, 
 	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
 	double sinW0 = std::sin(w0);
 	double cosW0 = std::cos(w0);
-	double alpha = sinW0 / (2 * std::cos(cascadedNum *  M_PI / (2 * numBiquads - 1)));
+	double alphaTheta = cascadedNum * M_PI / (2 * numBiquads - 1);
+	double alpha = sinW0 / (2 * std::cos(alphaTheta));
 	double a0, a1, a2, b0, b1;	//B2 = b0, so
 	a0 = 1 / (1 + alpha);		//Did reciprical so I can multiply instead of divide
 	a1 = (-2 * cosW0) * a0;
@@ -100,7 +102,7 @@ FilterCoef FilterDesign::Cascade1stAnd2ndOrderHighpass(FilterParameters params, 
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
-FilterCoef FilterDesign::Cascade1stAnd2ndOrderLowpass(FilterParameters params, int cascadedNum, unsigned int numBiquads)
+FilterCoef FilterDesign::Cascade1stAnd2ndOrderLowpass(FilterParameters& params, int cascadedNum, unsigned int numBiquads)
 {
 	if (!cascadedNum)	//The first filter in a 18/30/42 filter is a first order filter
 		return GetFirstOrderHighpass(params);
@@ -118,7 +120,7 @@ FilterCoef FilterDesign::Cascade1stAnd2ndOrderLowpass(FilterParameters params, i
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
-FilterCoef FilterDesign::GetCoefs(FilterParameters params)
+FilterCoef FilterDesign::GetCoefs(FilterParameters& params)
 {
 	switch (params.getFilterType())
 	{
