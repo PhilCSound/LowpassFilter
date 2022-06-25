68fac8410fa6204fc95f535aca426f35cffa588c
Phil C <philcsound@gmail.com>
2022-06-25 02:44:58 -0500

    Added peaking/low and high shelf

--- Source/FilterDesign.cpp
+++ Source/FilterDesign.cpp
@@ -50,6 +50,79 @@ FilterCoef FilterDesign::GetSecondOrderLowpass(FilterParameters& params)
 	return FilterCoef(1, a1, a2, b0, b1, b0);
 }
 
+FilterCoef FilterDesign::GetSecondOrderPeaking(FilterParameters & params)
+{
+	//params.getGainFactor() is in form A = 10^(db/40)
+	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
+	double sinW0 = std::sin(w0);
+	double cosW0 = std::cos(w0);
+	double alpha = sinW0 / (2 * params.getResonance());
+	double A = params.getGainFactor();
+	double a0, a2, b0, b1, b2;
+	a0 = 1 / (1 + alpha / A);
+
+	b0 = (1 + alpha * A) * a0;
+	b1 = -2 * cosW0 * a0;
+	b2 = (1 - alpha * A) * a0;
+	a2 = (1 - alpha / A) * a0;
+	return FilterCoef(1, b1, a2, b0, b1, b2); // a1 = b1
+}
+
+FilterCoef FilterDesign::GetSecondOrderAllpass(FilterParameters & params)
+{
+	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
+	double sinW0 = std::sin(w0);
+	double cosW0 = std::cos(w0);
+	double alpha = sinW0 / (2 * params.getResonance());
+	double a0, a1, a2;	//b2 and a0 = 1, a1 = b1, b0 = a2
+	a0 = 1 / (1 + alpha);		//Did reciprical so I can multiply instead of divide
+	a1 = -2 * cosW0 * a0;
+	a2 = (1 - alpha) * a0;
+	return FilterCoef(1, a1, a2, a2, a1, 1); 
+}
+
+FilterCoef FilterDesign::GetLowShelf(FilterParameters & params)
+{
+	//params.getGainFactor() is in form A = 10^(db/40)
+	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
+	double sinW0 = std::sin(w0);
+	double cosW0 = std::cos(w0);
+	double alpha = sinW0 / (2 * params.getResonance());
+	double A = params.getGainFactor();
+	double B = sqrt(A) / params.getResonance();
+
+	double a0, a1, a2, b0, b1, b2;
+	a0 = 1 / ((A + 1) + ((A - 1) * cosW0) + (B * sinW0));
+	a1 = -2 * ((A - 1) + ((A + 1) * cosW0)) * a0;
+	a2 = ((A + 1) + ((A - 1) * cosW0) - (B * sinW0)) * a0;
+	b0 = A * ((A + 1) - ((A - 1) * cosW0) + (B * sinW0)) * a0;
+	b1 = 2 * A * ((A - 1) - ((A + 1) * cosW0)) * a0;
+	b2 = A * ((A + 1) - ((A - 1) * cosW0) - (B * sinW0)) * a0;
+
+	return FilterCoef(1, a1, a2, b0, b1, b2);
+}
+
+FilterCoef FilterDesign::GetHighShelf(FilterParameters & params)
+{
+	//params.getGainFactor() is in form A = 10^(db/40)
+	double w0 = 2 * M_PI * params.getCutoff() / params.getSampleRate();
+	double sinW0 = std::sin(w0);
+	double cosW0 = std::cos(w0);
+	double alpha = sinW0 / (2 * params.getResonance());
+	double A = params.getGainFactor();
+	double B = sqrt(A) / params.getResonance();
+
+	double a0, a1, a2, b0, b1, b2;
+	a0 = 1 / ((A + 1) - ((A - 1) * cosW0) + (B * sinW0));
+	a1 = 2 * ((A - 1) - ((A + 1) * cosW0)) * a0;
+	a2 = ((A + 1) - ((A - 1) * cosW0) - (B * sinW0)) * a0;
+	b0 = A * ((A + 1) + ((A - 1) * cosW0) + (B * sinW0)) * a0;
+	b1 = -2 * A * ((A - 1) + ((A + 1) * cosW0)) * a0;
+	b2 = A * ((A + 1) + ((A - 1) * cosW0) - (B * sinW0)) * a0;
+
+	return FilterCoef(1, a1, a2, b0, b1, b2);
+}
+
 //Returns coefs for 12/24/36/48db flat filters
 //Cascaded num = index of filter, num2ndOrder is max num biquads cascading ex: 48db is 4 biquads
 FilterCoef FilterDesign::CascadeSecondOrderHighpass(FilterParameters& params, int cascadedNum, unsigned int num2ndOrdFilter)
@@ -126,18 +199,23 @@ FilterCoef FilterDesign::GetCoefs(FilterParameters& params)
 	{
 	case RESONANT_LOWPASS:
 		return GetSecondOrderLowpass(params);
-		break;
 
 	case RESONANT_HIGHPASS:
 		return GetSecondOrderHighpass(params);
-		break;
 
 	case ALLPASS:
-		return FilterCoef::GetBlankCoef();
-		break;
+		return GetSecondOrderAllpass(params);
+
+	case PEAKING:
+		return GetSecondOrderPeaking(params);
+
+	case HIGHSHELF:
+		return GetHighShelf(params);
+	
+	case LOWSHELF:
+		return GetLowShelf(params);
 
 	default:
 		return GetSecondOrderLowpass(params);
-		break;
 	}
 }
--- Source/FilterDesign.h
+++ Source/FilterDesign.h
@@ -11,8 +11,6 @@
 #pragma once
 #include "FilterParameters.h"
 #include "FilterCoef.h"
-#define _USE_MATH_DEFINES
-#include <cmath>
 #include <algorithm>
 
 namespace FilterDesign
@@ -21,6 +19,10 @@ namespace FilterDesign
 	FilterCoef GetFirstOrderLowpass(FilterParameters& params);
 	FilterCoef GetSecondOrderHighpass(FilterParameters& params);
 	FilterCoef GetSecondOrderLowpass(FilterParameters& params);
+	FilterCoef GetSecondOrderPeaking(FilterParameters& params);
+	FilterCoef GetSecondOrderAllpass(FilterParameters& params);
+	FilterCoef GetLowShelf(FilterParameters& params);
+	FilterCoef GetHighShelf(FilterParameters& params);
 	FilterCoef CascadeSecondOrderHighpass(FilterParameters& params, int cascadedNum = 0, unsigned int num2ndOrdFilter = 1);
 	FilterCoef CascadeSecondOrderLowpass(FilterParameters& params, int cascadedNum = 0, unsigned int num2ndOrdFilter = 1);
 	FilterCoef Cascade1stAnd2ndOrderHighpass(FilterParameters& params, int cascadedNum = 0, unsigned int numBiquads = 1);
